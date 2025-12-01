e60f8c1a19385445045431d1f1492c5d7cbb3b0e
vaibhav chaturvedi <vaibhavchaturvedi.work@gmail.com>
2025-12-01 23:37:19 +0530

    updating logic

--- backend/index.js
+++ backend/index.js
@@ -4,18 +4,17 @@ const cors = require('cors');
 require('dotenv').config();
 
 const authRoutes = require('./routes/auth');
+const firRoutes = require('./routes/FIR');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', authRoutes);
+app.use('/api', firRoutes);
 
-// MongoDB Connection
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
